c784c9a2eaa0805083a074554f1a6a47bcdd4db6
Charles Wood <chuck@lostjungles.com>
2009-04-04 16:13:30 -0600

    Make code match docs

--- lib/migration_helpers.rb
+++ lib/migration_helpers.rb
@@ -7,13 +7,13 @@ module ActiveRecord
     
     end
 
-    def self.remove_foreign_key(from_table, from_column)
+    def self.drop_foreign_key(from_table, from_column)
       constraint_name ||= "fk_#{from_table}_#{from_column}"[0,64]
 
       execute %{alter table #{from_table} drop foreign key #{constraint_name}}
     end
 
-    def self.remove_foreign_key_by_constraint_name(from_table, constraint_name = nil)
+    def self.drop_foreign_key_by_constraint_name(from_table, constraint_name = nil)
       execute %{alter table #{from_table} drop foreign key #{constraint_name}}
     end
   end
