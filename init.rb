01412ed9b2ab2e6493430e0f7b2cd85f71b55b5d
Chuck Wood <chuck@pmamediagroup.com>
2009-05-15 11:30:10 -0600

    add foreign key stuff to MysqlAdapter

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
 require 'migration_helpers'
 
 ActiveRecord::Migration.send :include, MigrationHelpers::ForeignKeyMigrations
+ActiveRecord::ConnectionAdapters::MysqlAdapter.send :include, MigrationHelpers::ForeignKeyMigrations
